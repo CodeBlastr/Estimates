c6264814afbe58b005ee47e43a8dedf6911f19b0
Richard Kersey <richard@razorit.com>
2013-10-16 20:14:57 -0400

    removed copy and pasted comments that don't match the file

--- View/Helper/EstimateHelper.php
+++ View/Helper/EstimateHelper.php
@@ -1,22 +1,5 @@
 <?php
-/**
- * Copyright 2010, Cake Development Corporation (http://cakedc.com)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright Copyright 2010, Cake Development Corporation (http://cakedc.com)
- * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
 
-/**
- * CakePHP Ratings Plugin
- *
- * Rating helper
- *
- * @package 	ratings
- * @subpackage 	ratings.views.helpers
- */
 class EstimateHelper extends AppHelper {
 
 /**
